3808ca951882fa673ae12abbae22fb11b6bd2ac9
TONY <tony.com>
2025-09-13 09:42:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
-import ButtonClickExample from "./ButtonClickExample";
 function App() {
+ const name = "React Developer";
+ const course = "Full Stack Development";
+ // Using string literals with backticks
+ const message = `Hello, ${name}! Welcome to the ${course} course.`;
  return (
- <div>
- <ButtonClickExample />
+ <div style={{ textAlign: "center", marginTop: "50px" }}>
+ <h1>{message}</h1>
+ <p>{`This text is displayed using string literals in React.`}</p>
  </div>
  );
 }
